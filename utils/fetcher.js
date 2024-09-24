f5ac912fe3ae0b09840ee7ae1e411d2b08f01e8b
haroon6268 <136369742+haroon6268@users.noreply.github.com>
2024-09-24 17:37:28 -0400

    Fixed API URL

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -2,7 +2,7 @@ const fetcher = async (url = "", options = {}) => {
   const baseUrl = "https://aiworkout-nine.vercel.app";
   const devUrl = "http://localhost:3000";
 
-  let res = await fetch(`${devUrl}${url}`, { ...options });
+  let res = await fetch(`${baseUrl}${url}`, { ...options });
   res = await res.json();
   return res;
 };
